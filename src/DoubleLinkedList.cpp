d65231d96cfb374525f05b21c539290600abb888
Tim Ashton <t.j.ashton1@gmail.com>
2015-04-24 17:56:15 +1000

    Update DoubleLinkedList.cpp

--- src/DoubleLinkedList.cpp
+++ src/DoubleLinkedList.cpp
@@ -11,17 +11,19 @@ class DoubleLinkedList::ListImpl
 {
 	struct Node
 	{
+		int data;
+		std::shared_ptr<Node> next;
+		std::shared_ptr<Node> previous;
+
 		Node(int input)
 			:data(input)
+			, next(NULL)
+			,previous(NULL)
 		{}
-
-		int data;
-		Node *next;
-		Node *previous;
 	};
 
-	Node *firstNode;
-	Node *lastNode;
+	std::shared_ptr<Node> firstNode;
+	std::shared_ptr<Node> lastNode;
 	int listSize;
 
 public:
@@ -33,9 +35,7 @@ public:
 	}
 
 	~ListImpl()
-	{
-		// TODO
-	}
+	{} // Let the shared_ptr handle delete.
 
 	/*
 	* int Size()
@@ -48,23 +48,49 @@ public:
 	}
 
 	/*
-	* insert()
+	* Insert()
 	*
 	* Inserts a new node populated with input data in the
-	* correct location in the list. Maintains ordering of
-	* list.
+	* correct location in the list. Maintains ordering of list.
+	* If an attempt to insert a value that already resides in
+	* the list insert will insert in the front of existing node(s).
 	*/
 	void Insert(int input)
 	{
-		if (!firstNode)
+		std::shared_ptr<Node> newNode(new Node(input));
+
+		if (!firstNode.get()) //insert into an empty list
 		{
-			firstNode = new Node(input);
-			lastNode = firstNode;
+			firstNode = newNode;
+			lastNode = newNode;
 			listSize++;
 		}
 		else
 		{
-			// Find the location to insert the node.
+			Insert(newNode, firstNode);
+		}
+	}
+
+private:
+	/*
+	* Insert()
+	*
+	* This is the recursive insert on the list.
+	* Note: There is absolutely no need for recuresion. In fact this would be more 
+	* econimic to just us a while loop. 
+	* 
+	* I just wanted to use recursion for fun...
+	*/
+	void Insert(std::shared_ptr<Node> newNode, std::shared_ptr<Node> next)
+	{
+		if (newNode->data < next->data)
+		{
+			Insert(newNode, next->next);
+		}
+		else
+		{
+			newNode->next = next;
+			newNode->previous = next->previous;
 		}
 	}
 };
