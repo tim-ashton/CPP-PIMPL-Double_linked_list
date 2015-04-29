c45d6301eeb69db9eb0eb31403515c9510fdbee1
Tim Ashton <t.j.ashton1@gmail.com>
2015-04-29 16:42:59 +1000

    Update DoubleLinkedList.cpp

--- src/DoubleLinkedList.cpp
+++ src/DoubleLinkedList.cpp
@@ -7,14 +7,12 @@
  *      Author: Tim Ashton
  */
 
-#include <cstddef>
-#include <type_traits>
-
 
 #include "DoubleLinkedList.h"
 
 class DoubleLinkedList::ListImpl
 {
+
 	struct Node
 	{
 		int data;
@@ -22,9 +20,9 @@ class DoubleLinkedList::ListImpl
 		std::shared_ptr<Node> previous;
 
 		Node(int input)
-		:data(input)
-		, next(NULL)
-		, previous(NULL)
+			:data(input)
+			, next(NULL)
+			, previous(NULL)
 		{}
 	};
 
@@ -33,122 +31,28 @@ class DoubleLinkedList::ListImpl
 	int listSize;
 
 public:
-
-	class ListIterator
-	: public std::iterator<std::forward_iterator_tag,
-	  Node,
-	  std::ptrdiff_t,
-	  Node*,
-	  Node&>
-	{
-	private:
-		Node *current;
-	public:
-
-		// Default constructor (gives end)
-		ListIterator()
-		:current(nullptr)
-		{}
-
-		// Copy Constructor
-		explicit ForwardIterator(Node* n)
-		: current(n)
-		{
-		}
-
-		// Swap
-		void swap(ListIterator& other) noexcept
-				{
-			std::swap(current, other.current);
-				}
-
-		//TODO
-		// Assignment operator
-
-		// Dereference (Read/Write)
-		int& operator*()
-		{
-			return current->data;
-		}
-
-		// Dereference const
-		const int& operator*() const
-		{
-			return current->data;
-		}
-
-		// Pre-increment
-		ListIterator& operator++()
-						{
-			if ( current )
-			{
-				current = current->next.get();
-			}
-			return *this;
-						}
-
-		// Post-increment
-		ListIterator operator++(int)
-						{
-			ListIterator temp = (*this);
-			current = current->next.get();
-			return temp;
-						}
-
-		//TODO
-		// Postincrement and de-reference
-		//		int& operator++()
-		//		{
-		//			ListIterator temp = (*this);
-		//			current = current->next.get();
-		//			return temp.current->data;
-		//		}
-
-		//TODO
-		// Postincrement and assignment
-
-		bool operator==(const ListIterator& iter) const
-						{
-			return current == iter.current;
-						}
-
-		bool operator!=(const ListIterator& iter) const
-						{
-			return current != iter.current;
-						}
-
-
-		//		Member accesses (-> when de-referencing returns an object with members).
-
-
-	};
-
-	typedef ListIterator< int, Node > iterator;
-	//typedef ListIterator< int const, Node > const > const_iterator;
-
 	ListImpl()
-	{
-		listSize = 0;
-	}
+		:listSize(0)
+	{}
 
 	~ListImpl()
 	{} // Let the shared_ptr handle delete.
 
 	/*
-	 * int Size()
-	 *
-	 * Return the number of nodes in the list.
-	 */
+	* int Size()
+	*
+	* Return the number of nodes in the list.
+	*/
 	int Size()
 	{
 		return listSize;
 	}
 
 	/*
-	 * Insert()
-	 *
-	 * Inserts a new node populated with input data in the list.
-	 */
+	* Insert()
+	*
+	* Inserts a new node populated with input data in the list.
+	*/
 	void Insert(int input)
 	{
 		std::shared_ptr<Node> newNode(new Node(input));
@@ -172,10 +76,10 @@ public:
 	}
 
 	/*
-	 * Remove
-	 *
-	 * Removes the node(s) where value matches node data.
-	 */
+	* Remove
+	*
+	* Removes the node(s) where value matches node data.
+	*/
 	void Remove(int value)
 	{
 		if (!firstNode)
@@ -202,48 +106,41 @@ public:
 	}
 
 	/*
-	 * Begin
-	 */
-	ListIterator Begin()
-	{
-
-	}
-
-	/*
-	 *Empty
-	 *
-	 * Return true if the container size is 0, false otherwise.
-	 */
+	*Empty
+	*
+	* Return true if the container size is 0, false otherwise.
+	*/
 	bool Empty()
 	{
 		return (listSize == 0);
 	}
 
 	/*
-	 * Back
-	 *
-	 * Return value of data in the last node in the list.
-	 */
+	* Back
+	*
+	* Return value of data in the last node in the list.
+	*/
 	int Back()
 	{
 		return lastNode->data;
 	}
 
 	/*
-	 * Return value of data in the first node in the list.
-	 */
+	* Return value of data in the first node in the list.
+	*/
 	int Front()
 	{
 		return firstNode->data;
 	}
 
-	private:
+	
+private:
 
 	/*
-	 * Insert()
-	 *
-	 * Recursive insert on the list.
-	 */
+	* Insert()
+	*
+	* Recursive insert on the list.
+	*/
 	std::shared_ptr<Node> Insert(std::shared_ptr<Node> newNode, std::shared_ptr<Node> head)
 	{
 		if (!head->next)
@@ -260,16 +157,16 @@ public:
 	}
 
 	/*
-	 * Remove
-	 *
-	 * Recursive helper function to remove a node in the list.
-	 */
+	* Remove
+	*
+	* Recursive helper function to remove a node in the list.
+	*/
 	std::shared_ptr<Node> Remove(int value, std::shared_ptr<Node> head)
-																			{
-		if(head->data == value) //remove head
+	{
+		if (head->data == value) //remove head
 		{
 			head->previous->next = head->next;
-			if(head == lastNode)
+			if (head == lastNode)
 			{
 				lastNode = head->previous;
 			}
@@ -286,18 +183,17 @@ public:
 			Remove(value, head->next);
 		}
 		return head->previous;
-																			}
+	}
 };
 
+
+
 DoubleLinkedList::DoubleLinkedList()
-: pImpl(new ListImpl())
+	: pImpl(new ListImpl())
 {}
 
 DoubleLinkedList::~DoubleLinkedList()
-{
-	// Do nothing. Let the shared_ptr handle deallocation.
-}
-
+{} // Do nothing. Let the unique_ptr handle deallocation.
 
 int DoubleLinkedList::Size()
 {
@@ -328,5 +224,6 @@ int DoubleLinkedList::Front()
 {
 	return pImpl->Front();
 }
+
 
 
